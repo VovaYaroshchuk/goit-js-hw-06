6b834b74784c62cc456bb43401e1d6b532061869
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-05-16 14:11:58 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const ingredientList = document.createElement('li')
+for (const ingredient of ingredients) {
+  const ingredientList = document.createElement('li');
+ ingredientList.textContent = ingredient;
+ ingredientList.classList.add('item');
+ ingredientsListEl.appendChild(ingredientList, ingredientsListEl.firstElementChild)
+  }
+ 
