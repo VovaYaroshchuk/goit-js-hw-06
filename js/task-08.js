394c4c93a89fb3d04d89e3ce170964a62c526190
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-05-16 20:19:29 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputEmail = document.querySelector('[name="email"]')
+const inputPass = document.querySelector('[name="password"]')
+const inputForm = document.querySelector('.login-form')
 
+inputForm.addEventListener('submit', formSubit)
+
+function formSubit(event) {
+    event.preventDefault()
+    if (inputEmail.value === '' || inputPass.value === '')
+    {alert('Увага! Всі поля повинні бути заповнені')}
+    else {
+        const email = event.currentTarget.elements.email.value;
+        const pass = event.currentTarget.elements.password.value;
+        console.log({email, pass});
+
+        event.currentTarget.reset()
+    }
+}
